c6a8169510670bc2f2d8f5c1a3058e22fdc5c7cf
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2018-07-30 16:05:44 +0800

    Implemented firebase authentication for google.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,18 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const inlinesource = require('gulp-inline-source');
+const babelMinify = require('gulp-babel-minify');
+
+var htmlFilesToIgnore = ['!src/test.html'];
+
+gulp.task('minify-html', function () {
+    htmlFilesToIgnore.push('src/*.html');
+
+    return gulp.src(htmlFilesToIgnore)
+        .pipe(inlinesource({ compress: true }))
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('public'));
+});
