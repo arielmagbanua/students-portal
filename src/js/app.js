037b29792e85c24f01e928ddf944cd3eae9f6a7e
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2018-07-30 17:01:53 +0800

    Added app.js

---
+++ src/js/app.js
@@ -0,0 +1,49 @@
+(() => {
+    'use strict';
+
+    // Initialize Firebase
+    const config = {
+        apiKey: "AIzaSyC1EQPiocY-6hu8SKKtkXesAn-zObxzZrs",
+        authDomain: "students-portal-ad4ec.firebaseapp.com",
+        databaseURL: "https://students-portal-ad4ec.firebaseio.com",
+        projectId: "students-portal-ad4ec",
+        storageBucket: "students-portal-ad4ec.appspot.com",
+        messagingSenderId: "132986949363"
+    };
+    firebase.initializeApp(config);
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var phoneNumber = user.phoneNumber;
+            var providerData = user.providerData;
+
+            console.log(displayName);
+            console.log(email);
+            // user.getIdToken().then(function (accessToken) {
+            //     document.getElementById('sign-in-status').textContent = 'Signed in';
+            //     document.getElementById('sign-in').textContent = 'Sign out';
+            //     document.getElementById('account-details').textContent = JSON.stringify({
+            //         displayName: displayName,
+            //         email: email,
+            //         emailVerified: emailVerified,
+            //         phoneNumber: phoneNumber,
+            //         photoURL: photoURL,
+            //         uid: uid,
+            //         accessToken: accessToken,
+            //         providerData: providerData
+            //     }, null, '  ');
+            // });
+        } else {
+            console.log('No user logged in.');
+            window.location.replace('/login.html');
+        }
+    }, function (error) {
+        console.log(error);
+    });
+});
