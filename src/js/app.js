5bd78ec500fc45582e4f72e3cbdd78b0db5d2390
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2018-07-31 12:31:06 +0800

    Added eslint rules.

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,22 @@
 (() => {
     'use strict';
-
+    
     M.AutoInit();
 
     // Initialize Firebase
     const config = {
-        apiKey: "AIzaSyC1EQPiocY-6hu8SKKtkXesAn-zObxzZrs",
-        authDomain: "students-portal-ad4ec.firebaseapp.com",
-        databaseURL: "https://students-portal-ad4ec.firebaseio.com",
-        projectId: "students-portal-ad4ec",
-        storageBucket: "students-portal-ad4ec.appspot.com",
-        messagingSenderId: "132986949363"
+        apiKey: 'AIzaSyC1EQPiocY-6hu8SKKtkXesAn-zObxzZrs',
+        authDomain: 'students-portal-ad4ec.firebaseapp.com',
+        databaseURL: 'https://students-portal-ad4ec.firebaseio.com',
+        projectId: 'students-portal-ad4ec',
+        storageBucket: 'students-portal-ad4ec.appspot.com',
+        messagingSenderId: '132986949363'
     };
+
     firebase.initializeApp(config);
 
     document.addEventListener('DOMContentLoaded', () => {
-        firebase.auth().onAuthStateChanged(function (user) {
+        firebase.auth().onAuthStateChanged((user) => {
             if (user) {
                 // User is signed in.
                 var displayName = user.displayName;
@@ -25,23 +26,6 @@
                 var uid = user.uid;
                 var phoneNumber = user.phoneNumber;
                 var providerData = user.providerData;
-
-                console.log(displayName);
-                console.log(email);
-                // user.getIdToken().then(function (accessToken) {
-                //     document.getElementById('sign-in-status').textContent = 'Signed in';
-                //     document.getElementById('sign-in').textContent = 'Sign out';
-                //     document.getElementById('account-details').textContent = JSON.stringify({
-                //         displayName: displayName,
-                //         email: email,
-                //         emailVerified: emailVerified,
-                //         phoneNumber: phoneNumber,
-                //         photoURL: photoURL,
-                //         uid: uid,
-                //         accessToken: accessToken,
-                //         providerData: providerData
-                //     }, null, '  ');
-                // });
                 
                 $('#loading-spinner').fadeOut('fast', () => {
                     // Show the content after user is authenticated and loading spinner is faded out.
@@ -52,7 +36,7 @@
                 console.log('No user logged in.');
                 window.location.replace('/login.html');
             }
-        }, function (error) {
+        }, (error) => {
             console.log(error);
         });
     });
