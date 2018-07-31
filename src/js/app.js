76c5a2af5b73f3d0ab8a527963134f034d1fa064
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2018-07-31 15:02:20 +0800

    Added basic query for a student record.

--- src/js/app.js
+++ src/js/app.js
@@ -15,19 +15,37 @@
 
     firebase.initializeApp(config);
 
+    const settings = {
+        /* your settings... */
+        timestampsInSnapshots: true
+    };
+
+    // Initialize Cloud Firestore through Firebase
+    var firestore = firebase.firestore();
+    firestore.settings(settings);
+
     document.addEventListener('DOMContentLoaded', () => {
-        firebase.auth().onAuthStateChanged((user) => {
+        firebase.auth().onAuthStateChanged(user => {
             if (user) {
                 // User is signed in.
                 var displayName = user.displayName;
                 var email = user.email;
                 var emailVerified = user.emailVerified;
-                var photoURL = user.photoURL;
-                var uid = user.uid;
-                var phoneNumber = user.phoneNumber;
-                var providerData = user.providerData;
+                // var photoURL = user.photoURL;
+                // var uid = user.uid;
+                // var phoneNumber = user.phoneNumber;
+                // var providerData = user.providerData;
                 
-                console.log(email);
+                let studentsRef = firestore.collection('students');
+                let studentPromise = studentsRef.where('email', '==', email).limit(1).get();
+                studentPromise.then(querySnapshot => {
+                    // snapshot by default returns multiple docs get only the first one
+                    const students = querySnapshot.docs.map(doc => doc.data());
+                    const student = students[0];
+                    // query the information of each courses.
+                    
+                    return student;
+                }).catch(error => console.error(error));
 
                 $('#loading-spinner').fadeOut('fast', () => {
                     // Show the content after user is authenticated and loading spinner is faded out.
@@ -38,8 +56,10 @@
                 console.log('No user logged in.');
                 window.location.replace('/login.html');
             }
-        }, (error) => {
-            console.log(error);
-        });
+        }, error => console.log(error));
     });
+
+    function renderStudent(student) {
+        console.log(student.courses[0]);
+    }
 })();
