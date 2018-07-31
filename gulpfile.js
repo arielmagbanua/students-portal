3b3cd9a0e6aa5721e4ad1a3f20d7125144eee9ab
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2018-07-31 13:43:33 +0800

    Added minify-js gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const babelMinify = require('gulp-babel-minify');
 
 var htmlFilesToIgnore = ['!src/test.html'];
 
-gulp.task('minify-html', function () {
+gulp.task('minify-html', function() {
     htmlFilesToIgnore.push('src/*.html');
 
     return gulp.src(htmlFilesToIgnore)
@@ -17,7 +17,7 @@ gulp.task('minify-html', function () {
         .pipe(gulp.dest('public'));
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     let pathsToCompile = [
         'src/sass/**/*.scss',
         '!src/sass/test.scss'
@@ -28,6 +28,33 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('src/css/'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', function() {
     gulp.watch('src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('minify-js', function() {
+    let pathsToMinify = [
+        '!src/js/jquery.min.js',
+        '!src/js/materialize.min.js',
+        'src/js/**/*.js'
+    ];
+
+    let awts = gulp.src(pathsToMinify)
+        .pipe(babelMinify({
+            mangle: {
+                keepClassName: true
+            }
+        }))
+        .pipe(gulp.dest('public/js/'));
+    
+    // Just copy
+    pathsToMinify = [
+        'src/js/jquery.min.js',
+        'src/js/materialize.min.js'
+    ];
+
+    awts = gulp.src(pathsToMinify)
+        .pipe(gulp.dest('public/js/'));
+    
+    return awts;
 });
