d91a63fd46d60939ce543c6f2c1eec9504e40291
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2018-08-02 17:24:28 +0800

    Added insertion of cards.

--- src/js/app.js
+++ src/js/app.js
@@ -31,18 +31,48 @@
                 var displayName = user.displayName;
                 var email = user.email;
                 var emailVerified = user.emailVerified;
-                
+
                 getStudent(firestore, email).then(studentData => {
                     // TODO: Build the cards here
+                    let gradesContainer = $('#grades');
+                    
                     document.querySelector('h4.greeting').innerHTML = studentData.student.name;
                     studentData.courses.forEach((item, index, arr) => {
-                        
+                        console.log(item);
+                        let courseSchool = '<p>School: ' + item.course.school + '</p></br>';
+                        let courseName = '<p>Course name: ' + item.course.name+'</p></br>';
+                        let courseCode = '<p>Course code: ' + item.course.code + '</p></br>';
+                        let courseSubjectCode = '<p>Subject Code: ' + item.course.subject_code + '</p></br>';
+
+                        let card = $(`  <div class="col s1 m6 l6 xl6">
+                                            <div class="card blue lighten-5">
+                                                <div class="card-content">
+                                                    `+ courseSchool + courseName + courseCode + courseSubjectCode+`
+                                                </div>
+                                                <div class="card-tabs">
+                                                    <ul class="tabs tabs-fixed-width">
+                                                        <li class="tab"><a href="#test4">Test 1</a></li>
+                                                        <li class="tab"><a class="active" href="#test5">Test 2</a></li>
+                                                        <li class="tab"><a href="#test6">Test 3</a></li>
+                                                    </ul>
+                                                </div>
+                                                <div class="card-content grey lighten-4">
+                                                    <div id="test4">Test 1</div>
+                                                    <div id="test5">Test 2</div>
+                                                    <div id="test6">Test 3</div>
+                                                </div>
+                                            </div>
+                                        </div>`);
+                        gradesContainer.append(card);
                     });
 
                     $('#loading-spinner').fadeOut('fast', () => {
                         // Show the content after user is authenticated and loading spinner is faded out.
                         document.getElementById('nav-bar').removeAttribute('hidden');
                         document.getElementById('main-content').removeAttribute('hidden');
+
+                        // Auto init for dynamically added elements
+                        M.AutoInit();
                     });
                 }).catch(error => console.error(error));
             } else {
@@ -62,8 +92,10 @@
     async function getStudent(firestore, email) {
         let studentsRef = firestore.collection('students');
         let studentPromise = await studentsRef.where('email', '==', email).limit(1).get();
+        
         // extract the student
         const student = await studentPromise.docs.map(doc => doc.data())[0];
+
         const coursePromises = await student.courses.map(async courseData => {
             const courseRef = await courseData.course.get();
             const courseInfo = await courseRef.data();
@@ -73,7 +105,7 @@
                 prelim: courseData.prelim,
                 midterm: courseData.midterm,
                 final: courseData.final
-            }
+            };
         });
 
         const courses = await Promise.all(coursePromises);
